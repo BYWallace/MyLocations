b777d61da3ce22ea771484b544a6b8ea2fed9685
Brett Wallace <bwallace90@gmail.com>
2016-03-20 22:04:39 -0400

    Set up CurrentLocation VC tab storyboard

---
+++ MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Brett Wallace on 3/20/16.
+//  Copyright © 2016 Brett Wallace. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+
+    @IBAction func getLocation() {
+        
+    }
+}
+
